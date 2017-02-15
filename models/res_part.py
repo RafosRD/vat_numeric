7a2894db1303f44feeb2b0e065b2a4de10d8695a
Raul Ovalle <raul@xmarts.do>
2017-02-15 11:39:14 -0400

    correccion de error

--- models/res_part.py
+++ models/res_part.py
@@ -6,13 +6,23 @@ class vat_numeric_res_partner(models.Model):
 
     _defaults = {'country_id': lambda self, cr, uid, context:self.pool.get('res.country').browse(cr, uid, self.pool.get('res.country').search(cr, uid, [('code', '=', 'DO')]))[0].id, }
 
-    @api.one
-    @api.constrains('vat','company_type')
-    def vat_is_numeric(self):
-        if self.country_id.code == 'DO':
-            if self.vat == True:
-                if self.vat.isdigit() == False:
-                    raise exceptions.ValidationError('Solo puede colocar numeros en el campo RNC/Cedula')
+    @api.model
+    def create(self,vals):
+        #raise exceptions.ValidationError(vals['country_id'])
+        if self.env['res.country'].browse(vals['country_id']).code == 'DO':
+            if vals['vat'].isdigit() == False:
+                raise exceptions.ValidationError('Solo puede colocar numeros en el campo RNC/Cedula')
+
+        return super(vat_numeric_res_partner, self).create(vals)
+
+    def update(self, vals):
+
+        if self.env['res.country'].browse(vals['country_id']).code == 'DO':
+            if vals['vat'].isdigit() == False:
+                raise exceptions.ValidationError('Solo puede colocar numeros en el campo RNC/Cedula')
+
+
+        super(vat_numeric_res_partner, self).update(vals)
 
 
 
