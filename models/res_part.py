2da06c3ccabf0b9d5bbc2c1e948d29632937af9b
Raul Ovalle <raul@xmarts.do>
2017-02-15 11:51:50 -0400

    2

--- models/res_part.py
+++ models/res_part.py
@@ -6,14 +6,14 @@ class vat_numeric_res_partner(models.Model):
 
     _defaults = {'country_id': lambda self, cr, uid, context:self.pool.get('res.country').browse(cr, uid, self.pool.get('res.country').search(cr, uid, [('code', '=', 'DO')]))[0].id, }
 
-    @api.model
-    def create(self,vals):
-        #raise exceptions.ValidationError(vals['country_id'])
-        if self.env['res.country'].browse(vals['country_id']).code == 'DO':
-            if vals['vat'].isdigit() == False:
-                raise exceptions.ValidationError('Solo puede colocar numeros en el campo RNC/Cedula')
+    @api.one
+    @api.constrains('vat','company_type')
+    def vat_is_numeric(self):
+        if self.country_id.code == 'DO':
+            if self.vat is not False:
+                if self.vat.isdigit() == False:
+                    raise exceptions.ValidationError('Solo puede colocar numeros en el campo RNC/Cedula')
 
-        return super(vat_numeric_res_partner, self).create(vals)
 
 
 
